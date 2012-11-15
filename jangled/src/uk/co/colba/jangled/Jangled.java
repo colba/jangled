723945b10e382ebf1286fc072834454e4dccb90c
colba <colba@c1847t.rx>
2012-11-15 21:13:54 +0000

    test2

---
+++ jangled/src/uk/co/colba/jangled/Jangled.java
@@ -0,0 +1,9 @@
+package uk.co.colba.jangled;
+
+public class Jangled {
+
+	public Jangled() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ jangled/src/uk/co/colba/jangled/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+/**
+ * @author Colin - ColBa
+ * @version 0.0.1
+ * 
+ * Java / JavaScript Mangled == Jangled
+ *
+ */
+package uk.co.colba.jangled;
+
